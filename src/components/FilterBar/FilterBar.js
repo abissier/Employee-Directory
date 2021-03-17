a9ff0926533a49867c8eeadc78e85b116be5172b
abissier <71886314+abissier@users.noreply.github.com>
2021-03-16 21:46:39 -0700

    corrected filterbar

--- src/components/FilterBar/FilterBar.js
+++ src/components/FilterBar/FilterBar.js
@@ -2,7 +2,27 @@ import React from 'react';
 import './style.css';
 
 function FilterBar() {
-	return <h1 className="filter">Filter Here</h1>;
+	return (
+		<div class="filter-wrapper">
+			<div class="filter">
+				<h3>Sort By</h3>
+				<div class="form-check">
+					<input class="form-check-input" type="checkbox" id="flexCheckDefault" value="name" />
+					<label class="form-check-label" for="flexCheckDefault">
+						Name
+					</label>
+				</div>
+				<div class="form-check">
+					<input class="form-check-input" type="checkbox" id="flexCheck2" value="country" />
+					<label class="form-check-label" for="flexCheckChecked">
+						Country
+					</label>
+				</div>
+				<h3> Search By</h3>
+				<input class="form-control" type="text" placeholder="username" />
+			</div>
+		</div>
+	);
 }
 
 export default FilterBar;
